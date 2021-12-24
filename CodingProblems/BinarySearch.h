4e8aafcd0bed5358872d4f30e441745d7324643b
Byungkyu Jun <32653425+coxozo123@users.noreply.github.com>
2021-12-25 02:29:49 +0900

    added 2352

--- CodingProblems/BinarySearch.h
+++ CodingProblems/BinarySearch.h
@@ -2,7 +2,7 @@
 
 
 // PROBLEM 3020
-
+/*
 int lower_3020[500001];
 int upper_3020[500001];
 int total_3020[500001];
@@ -48,8 +48,51 @@ void Problem_3020()
 
 	std::cout << inputBuffer[0] << " " << inputBuffer[1] << '\n';
 }
+*/
+
+// PROBLEM 2352
+
+int subsequence_2352[40001];
+
+int binarySearch_2352(int start, int end, int val)
+{
+	if (start == end) { return subsequence_2352[start] >= val ? start : start + 1; }
+	//if (start >= end) { return start; }
+	int mid = start + (end - start) / 2;
+	if (subsequence_2352[mid] == val) { return mid; }
+	else if (val < subsequence_2352[mid]) { return binarySearch_2352(start, mid, val); }
+	//else if (subsequence_2352[mid] > val) { return binarySearch_2352(start, mid - 1, val); }
+	else { return binarySearch_2352(mid + 1, end, val); }
+}
+
+void Problem_2352()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cin.tie(nullptr);
+
+	int N, result = 1, inputBuffer;
+	std::cin >> N;
+	
+	std::cin >> subsequence_2352[0];
+
+	while (--N > 0)
+	{
+		std::cin >> inputBuffer;
+		if (subsequence_2352[result - 1] < inputBuffer)
+		{
+			subsequence_2352[result++] = inputBuffer;
+		}
+		else
+		{
+			subsequence_2352[binarySearch_2352(0, result - 1, inputBuffer)] = inputBuffer;
+		}
+	}
+	
+	std::cout << result;
+}
 
 void ExecuteBinarySearch()
 {
-	Problem_3020();
+	//Problem_3020();
+	Problem_2352();
 }
