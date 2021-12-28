0abd1b3f0f9d4d17e6b66eef3b6c6908d9e4fe08
Byungkyu Jun <32653425+coxozo123@users.noreply.github.com>
2021-12-29 04:26:29 +0900

    added 23845

--- CodingProblems/Greedy.h
+++ CodingProblems/Greedy.h
@@ -1,6 +1,7 @@
 #pragma once
 
 // PROBLEM 1040
+/*
 std::string numStr_1040;
 int digitCounter_1040[10];
 long long N_1040, K_1040;
@@ -114,8 +115,87 @@ void Problem_1040()
 
 	std::cout << numStr_1040;
 }
+*/
+
+// PROBLEM 23845
+int data_23845[200001];
+int counts_23845[100001];
+long long result;
+
+inline void segProcess_23845(int start, long long end)
+{
+	if(start == end)
+	{
+		result += end * counts_23845[start];
+		return;
+	}
+	auto minmax = std::minmax_element(&counts_23845[start], (&counts_23845[end] + 1));
+	int minNum = minmax.first - counts_23845;
+	int maxCount = *minmax.second;
+	int minCount = *minmax.first;
+	if (minCount != 0)
+	{
+		for (int num = start; num <= end; num++)
+		{
+			counts_23845[num] -= minCount;
+		}
+
+		result += end * (end - start + 1) * minCount;
+	}
+
+	if (maxCount == minCount) { return; }
+	if(minNum - 1 >= start){ segProcess_23845(start, minNum - 1); }
+	if(minNum + 1 <= end){ segProcess_23845(minNum + 1, end); }
+}
+
+void Problem_23845()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	std::srand(std::time(nullptr));
+
+	int N, idxCounter = 0;
+	std::cin >> N;
+
+	double consumedTime = TimeChecker::CheckTime([&] {
+
+	while(N-- > 0)
+	{
+		//std::cin >> data_23845[idxCounter];
+		data_23845[idxCounter] = std::rand() % 100000;
+		counts_23845[data_23845[idxCounter]]++;
+		if (counts_23845[data_23845[idxCounter]] == 1) { idxCounter++; }
+	}
+	std::sort(data_23845, data_23845 + idxCounter, [](const int& lhs, const int& rhs) {return lhs > rhs; });
+
+	int startIdx = 0, endIdx = 0;
+	int prev = data_23845[0];
+	for (int idx = 1; idx < idxCounter; idx++)
+	{
+		if (prev - data_23845[idx] == 1)
+		{
+			endIdx++;
+		}
+		else
+		{
+			segProcess_23845(data_23845[endIdx], data_23845[startIdx]);
+			startIdx = endIdx = idx;
+		}
+
+		prev = data_23845[idx];
+	}
+	segProcess_23845(data_23845[endIdx], data_23845[startIdx]);
+
+	std::cout << result;
+
+		});
+
+	std::cout << " time : " << consumedTime << " ms";
+}
 
 void ExecuteGreedy()
 {
-	Problem_1040();
+	//Problem_1040();
+	Problem_23845();
 }
