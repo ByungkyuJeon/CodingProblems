516064b4ebe65c0d28dc4b128f3f83f2d5e8c3ba
Byungkyu Jun <32653425+coxozo123@users.noreply.github.com>
2022-01-19 11:40:55 +0900

    added 11505

--- CodingProblems/DataStructure/Tree.h
+++ CodingProblems/DataStructure/Tree.h
@@ -7,7 +7,7 @@ void Problem_11438()
 }
 
 // PROBLEM 1068
-
+/*
 std::vector<int> data[50];
 int inputs[50];
 
@@ -59,8 +59,77 @@ void Problem_1068()
 		std::cout << getLeaves(root);
 	}
 }
+*/
+
+// PROBLEM 11505
+
+long long data[1000002];
+long long segTree[4000000];
+
+long long makeSegTree(int start, int end, int node)
+{
+	if (start == end) { segTree[node] = data[start]; return segTree[node]; }
+	int mid = start + ((end - start) / 2);
+
+	segTree[node] = makeSegTree(start, mid, 2 * node) * makeSegTree(mid + 1, end, 2 * node + 1) % 1000000007;
+	return segTree[node];
+}
+
+long long modifySegTree(int start, int end, int node, int idx, long long val)
+{
+	if (start > idx || end < idx) { return segTree[node]; }
+
+	if (start == end && start == idx) { segTree[node] = val; return segTree[node]; }
+
+	int mid = start + ((end - start) / 2);
+	segTree[node] = modifySegTree(start, mid, 2 * node, idx, val) * modifySegTree(mid + 1, end, 2 * node + 1, idx, val) % 1000000007;
+	return segTree[node];
+}
+
+long long getSegNode(int start, int end, int node, int left, int right)
+{
+	if (left > end || right < start) { return 1; }
+
+	if (start >= left && end <= right) { return segTree[node]; }
+
+	int mid = start + ((end - start) / 2);
+	return getSegNode(start, mid, 2 * node, left, right) * getSegNode(mid + 1, end, 2 * node + 1, left, right) % 1000000007;
+}
+
+void Problem_11505()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cin.tie(nullptr);
+
+	int N, M, K, loopCount;
+	long long inputBuffer[3];
+	std::string outputStr;
+	std::cin >> N >> M >> K;
+	loopCount = M + K;
+	for (int idx = 1; idx <= N; idx++)
+	{
+		std::cin >> data[idx];
+	}
+	makeSegTree(1, N, 1);
+
+	while (loopCount-- > 0)
+	{
+		std::cin >> inputBuffer[0] >> inputBuffer[1] >> inputBuffer[2];
+		if (inputBuffer[0] == 1)
+		{
+			modifySegTree(1, N, 1, inputBuffer[1], inputBuffer[2]);
+		}
+		else
+		{
+			outputStr += std::to_string(getSegNode(1, N, 1, inputBuffer[1], inputBuffer[2])) + '\n';
+		}
+	}
+
+	std::cout << outputStr;
+}
 
 void ExecuteTree()
 {
-	Problem_1068();
+	//Problem_1068();
+	Problem_11505();
 }
