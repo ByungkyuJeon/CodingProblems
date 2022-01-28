a81bb7bfa5a54074930f9438ec740d404c1e9cef
Byungkyu Jun <32653425+coxozo123@users.noreply.github.com>
2022-01-28 22:33:15 +0900

    update main

--- CodingProblems/CodingProblems.cpp
+++ CodingProblems/CodingProblems.cpp
@@ -5,12 +5,16 @@
 #include "BaekjoonDataStructureProblems.h"
 #include "BaekjoonStringProblems.h"
 #include "BinarySearch.h"
+#include "BaekjoonSortProblems.h"
 
 #include "DataStructure/Tree.h"
 #include "DataStructure/Stack.h"
 #include "DataStructure/Queue.h"
 #include "DFS_BFS/DFS_BFS.h"
 #include "Greedy.h"
+#include "Math/Math.h"
+#include "DynamicProgramming.h"
+#include "Bit/Bit.h"
 
 int main()
 {
@@ -20,6 +24,7 @@ int main()
     ExecuteGraph();
     ExecuteDataStructure();
     ExecuteString();
+    ExecuteSort();
 
     ExecuteQueue();
     ExecuteTree();
@@ -27,5 +32,8 @@ int main()
     ExecuteBinarySearch();
     ExecuteDFS_BFS();
     ExecuteGreedy();
+    Execute_Math();
+    Execute_DynamicProgramming();
+    ExecuteBit();
 }
 
