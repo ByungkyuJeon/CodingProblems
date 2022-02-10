8ce0efe073585a45dd543717f48412a237b12c01
Byungkyu Jun <32653425+coxozo123@users.noreply.github.com>
2022-02-10 12:37:37 +0900

    added 15651

--- CodingProblems/BaekjoonBackTrackingProblems.h
+++ CodingProblems/BaekjoonBackTrackingProblems.h
@@ -48,7 +48,7 @@ void Problem_15649()
 */
 
 // PROBLEM 15650
-
+/*
 bool visited[9];
 std::vector<int> proc;
 int N, M, depth;
@@ -92,9 +92,51 @@ void Problem_15650()
 
 	std::cout << outputStr;
 }
+*/
+
+// PROBLEM 15651
+
+std::vector<int> proc;
+int N, M, depth;
+std::string outputStr;
+
+void process()
+{
+	if (depth == M)
+	{
+		for (const auto& elem : proc)
+		{
+			outputStr += std::to_string(elem) + " ";
+		}
+		outputStr += '\n';
+		return;
+	}
+
+	for (int idx = 1; idx <= N; idx++)
+	{
+		proc.emplace_back(idx);
+		depth++;
+		process();
+		proc.erase(proc.end() - 1);
+		depth--;
+	}
+}
+
+void Problem_15651()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cin.tie(nullptr);
+
+	std::cin >> N >> M;
+
+	process();
+
+	std::cout << outputStr;
+}
 
 void ExecuteBackTracking()
 {
 	//Problem_15649();
-	Problem_15650();
+	//Problem_15650();
+	Problem_15651();
 }
