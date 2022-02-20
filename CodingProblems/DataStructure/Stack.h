70a196447eb9601b3f14dbcd5f4980df96c5df06
Byungkyu Jun <32653425+coxozo123@users.noreply.github.com>
2022-02-20 12:15:41 +0900

    added 9012

--- CodingProblems/DataStructure/Stack.h
+++ CodingProblems/DataStructure/Stack.h
@@ -77,8 +77,37 @@ void Problem_10773()
 }
 */
 
+// PROBLEM 9012
+
+void Problem_9012()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cin.tie(nullptr);
+
+	int N, count;
+	bool check;
+	std::cin >> N;
+	std::string inputStr, outputStr;
+
+	while (N-- > 0)
+	{
+		std::cin >> inputStr;
+		count = 0; check = true;
+		for (int idx = inputStr.size() - 1; idx >= 0; idx--)
+		{
+			if (inputStr[idx] == ')'){ count++; }
+			else { if (--count < 0) { check = false; break; } }
+		}
+		if (check && count == 0) { outputStr += "YES\n"; }
+		else{ outputStr += "NO\n"; }
+	}
+
+	std::cout << outputStr;
+}
+
 void ExecuteStack()
 {
 	//Problem_10828();
 	//Problem_10773();
+	Problem_9012();
 }
