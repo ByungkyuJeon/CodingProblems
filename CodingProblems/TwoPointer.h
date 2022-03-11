c69738c3a3c502611e43e64eda82862497216fae
Byungkyu Jun <32653425+coxozo123@users.noreply.github.com>
2022-03-11 17:03:27 +0900

    added 1806

--- CodingProblems/TwoPointer.h
+++ CodingProblems/TwoPointer.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <numeric>
+
 // PROBLEM 3273
 /*
 void Problem_3273()
@@ -41,7 +43,7 @@ void Problem_3273()
 */
 
 // PROBLEM 2470
-
+/*
 void Problem_2470()
 {
 	std::ios_base::sync_with_stdio(0);
@@ -85,9 +87,55 @@ void Problem_2470()
 
 	std::cout << res[0] << " " << res[1];
 }
+*/
+
+// PROBLEM 1806
+
+void Problem_1806()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cin.tie(nullptr);
+
+	int N, S;
+	std::cin >> N >> S;
+	std::vector<int> data(N);
+	long long sum = 0;
+	for (int idx = 0; idx < N; idx++)
+	{
+		std::cin >> data[idx];
+		sum += data[idx];
+	}
+
+	if (sum >= S)
+	{
+		int sIdx = 0, eIdx = 0;
+		int min = N;
+		sum = data[sIdx];
+		while (true)
+		{
+			if (sum >= S && min > eIdx - sIdx + 1) { min = eIdx - sIdx + 1; }
+			if (sIdx != eIdx && sum >= S)
+			{
+				sum -= data[sIdx++];
+			}
+			else
+			{
+				if (eIdx == N - 1) { break; }
+				sum += data[++eIdx];
+			}
+		}
+
+		std::cout << min;
+	}
+	else
+	{
+		std::cout << 0;
+	}
+}
 
 void executeTwoPointer()
 {
 	//Problem_3273();
-	Problem_2470();
+	//Problem_2470();
+	Problem_1806();
 }
